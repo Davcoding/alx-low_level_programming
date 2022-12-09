099183b4e903e2c463f96e8baebb3793c8e086ca
Davcoding <tomiadeyemi@gmail.com>
2022-12-08 16:41:01 -0800

    Task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry
+ * main - prints the size of various types
  *
- * Return: Always 0
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
